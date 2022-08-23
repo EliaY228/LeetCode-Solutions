c295588a7de62f7ba8f381c504ff7df965cfda98
Elia Yermakov <eliayermakov@outlook.com>
2022-08-23 09:49:40 +0300

    Time: 109 ms (43.90%), Space: 42.5 MB (26.83%) - LeetHub

---
+++ 492-construct-the-rectangle/492-construct-the-rectangle.js
@@ -0,0 +1,12 @@
+var constructRectangle = function(area) {
+    
+let mid = Math.floor(Math.sqrt(area));
+
+  while (mid !== 1) {
+    if (area % mid === 0) {
+      return [area / mid, mid];
+    }
+    mid--;
+  }
+  return [area, 1];
+};
